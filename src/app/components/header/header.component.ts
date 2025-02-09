f4d7b309e47c718e3cf33271a18a5431af21adf9
Diego <diego171104@gmail.com>
2025-02-09 19:45:08 -0300

    ultimo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
-import { ProjectsComponent } from '../projects/projects.component';
-import { AboutComponent } from '../about/about.component';
-import { ContactoComponent } from '../contacto/contacto.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, HomeComponent, ProjectsComponent, AboutComponent, ContactoComponent],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
